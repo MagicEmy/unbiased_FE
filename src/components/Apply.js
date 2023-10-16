011d2f2d80371e0ce7dec9bdf1f55c4bc5b526d3
domi <dmaessen@student.codam.nl>
2023-10-16 17:01:21 +0200

    reworked components

--- src/components/Apply.js
+++ src/components/Apply.js
@@ -3,7 +3,14 @@ import '../App.css';
 function Apply() {
   return (
     <div className="App">
-      
+     <h1>Apply</h1>
+	 <h2>Company name</h2>
+	 <h2>Job title</h2>
+	 <div className="ID1">
+	  	<input type="text" placeholder="Full name"/>
+	  </div>
+	  <button>Upload CV</button>
+	  <button>Submit</button>
     </div>
   );
 }
--- src/components/Create.js
+++ src/components/Create.js
@@ -4,8 +4,15 @@ function Create() {
   return (
     <div className="App">
       <h1>Create application</h1>
-	  <input type="text" placeholder="Company name"/>
-	  <input type="text" placeholder="Job title"/>
+	  <div className="ID1">
+	  	<input type="text" placeholder="Company name"/>
+	  </div>
+	  <div className="ID2">
+	  	<input type="text" placeholder="Job title"/>
+	  </div>
+	  <div className="ID3">
+	  <input type="text" placeholder="Email HR-manager"/>
+	  </div>
 	  <button>Submit</button>
     </div>
   );
