78e2015623718eb08f8308ff933db4a6477d08d5
domi <dmaessen@student.codam.nl>
2023-10-17 11:42:49 +0200

    log created for Create.js

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,27 +1,46 @@
 import '../App.css';
-
-function onSubmit(){
-
-}
+import {useState} from 'react'
+import axios from 'axios';
 
 function Create() {
+	const[companyName, setCompanyName]=useState("");
+	const[jobTitle, setJobTitle]=useState("");
+	const[companyEmail, setCompanyEmail]=useState("");
+
+	function onSubmit(e){
+		e.preventDefault();
+		let jobPost={
+			companyName: companyName,
+			jobTitle: jobTitle,
+			companyEmail: companyEmail,
+		};
+	// 	axios.post(`https://jsonplaceholder.typicode.com/users`, { jobPost })
+    //   		.then(res => {
+    //     		console.log(res);
+    //    			console.log(res.data);
+    //   });
+		console.log(jobPost);
+	}
+
   return (
     <div className="App">
       <h1>Create application</h1>
-	  <form onSubmit={e => {handleSubmit(e)}}>
+	  <form onSubmit={e => {onSubmit(e)}}>
 	  <div className="ID1">
-	  	<input type="text" placeholder="Company name"/>
+	  	<input type="text" placeholder="Company name" onChange={e => setCompanyName(e.target.value)}/>
 	  </div>
 	  <div className="ID2">
-	  	<input type="text" placeholder="Job title"/>
+	  	<input type="text" placeholder="Job title" onChange={e => setJobTitle(e.target.value)}/>
 	  </div>
 	  <div className="ID3">
-	  <input type="text" placeholder="Email HR-manager"/>
+	  <input type="text" placeholder="Email HR-manager" onChange={e => setCompanyEmail(e.target.value)}/>
 	  </div>
-	  <button onClick={onsubmit}>Submit</button>
+	  <button type="submit" value="Submit">Submit</button>
 	  </form>
     </div>
   );
 }
 
 export default Create;
+
+// {} []
